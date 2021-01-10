4c3c1565f8bcedfc738276ff8968f3cf4ecad8d5
zhangblue <zhangdi013@sina.com>
2021-01-10 19:01:46 +0800

    add sin to kafka,redis,es demo

--- flink-learning-scala/src/main/scala/com/zhangdi/flink/scala/source/kafka/SensorSourceFromKafkaExample1.scala
+++ flink-learning-scala/src/main/scala/com/zhangdi/flink/scala/source/kafka/SensorSourceFromKafkaExample1.scala
@@ -7,12 +7,12 @@ import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
 
 /**
- * @description: ${todo}
+ * @description: 消费kafka，默认消费没有key
  * @author zhangdi
  * @date 2021/1/7 上午12:59
  * @since ${since}
  **/
-object SensorSourceFromKafka {
+object SensorSourceFromKafkaExample1 {
 
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
