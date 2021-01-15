f47ec0bb07fa7b966918166be7548b237dcc26ff
zhangblue <zhangdi013@sina.com>
2021-01-15 14:16:26 +0800

    fix

--- flink-learning-scala/src/main/scala/com/zhangdi/flink/scala/apitest/checkpoint/CheckPointRocksDBDemo.scala
+++ flink-learning-scala/src/main/scala/com/zhangdi/flink/scala/apitest/checkpoint/CheckPointRocksDBDemo.scala
@@ -6,20 +6,20 @@ import org.apache.flink.api.common.functions.MapFunction
 import org.apache.flink.api.common.restartstrategy.RestartStrategies
 import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.api.common.time.Time
-import org.apache.flink.runtime.state.filesystem.FsStateBackend
+import org.apache.flink.contrib.streaming.state.RocksDBStateBackend
 import org.apache.flink.streaming.api.CheckpointingMode
 import org.apache.flink.streaming.api.environment.CheckpointConfig
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
 
 /**
- * @description: flink checkpoint 到HDFS
+ * @description:
+ * 将task-manager内存使用rocksDB,最终的checkpoint依然是存储在hdfs上的
  * @author zhangdi
- * @date 2021/1/15 上午12:25
+ * @date 2021/1/15 上午11:48
  * @since ${since}
  **/
-object CheckPointToHDFSDemo {
-
+object CheckPointRocksDBDemo {
   def main(args: Array[String]): Unit = {
 
     val brokers = "172.16.36.123:9092"
@@ -29,7 +29,8 @@ object CheckPointToHDFSDemo {
 
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
-    env.setStateBackend(new FsStateBackend("hdfs://172.16.36.134:8020/zhangd/flink/checkpoint",true))
+//    env.setStateBackend(new RocksDBStateBackend("file:////Users/zhangdi/Downloads/tmp", true))
+            env.setStateBackend(new RocksDBStateBackend("hdfs://172.16.36.134:8020/zhangd/flink/checkpoint",true))
     env.enableCheckpointing(5000)
     env.getCheckpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE)
     //两次checkpoint的时间间隔
@@ -50,7 +51,7 @@ object CheckPointToHDFSDemo {
       override def map(value: String): String = {
         "nx_" + value
       }
-    })
+    }).print("value = ")
 
 
     env.execute("CheckPointToHDFSDemo")
@@ -64,5 +65,4 @@ object CheckPointToHDFSDemo {
 
     new FlinkKafkaConsumer[String](topic, new SimpleStringSchema, properties)
   }
-
 }
